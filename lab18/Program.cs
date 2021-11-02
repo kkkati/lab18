f8579de2ab5817c88d8b27460916dd7fb37f422c
user <user@WIN-2QU304JCFKP>
2021-11-02 13:30:07 +0200

    Добавьте файлы проекта.

---
+++ lab18/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> skobki = new Stack<string>();
+            Console.WriteLine("Введите выражение со скобками");
+            string str = Console.ReadLine();
+            for (int i = 0;i<str.Length;i++)
+            {
+                if (str[i] =='{')
+                {
+                    skobki.Push("}");
+                }
+                else
+                { 
+                    if (str[i]=='[')
+                    {
+                        skobki.Push("]");
+                    }
+                    else
+                    {
+                        if (str[i]=='(')
+                        {
+                            skobki.Push(")");
+                        }
+                        else
+                        {
+                            if (str[i] == '}')
+                            {
+                                if (skobki.Count == 0)
+                                {
+                                    Console.WriteLine("Скобки расставлены не правильно");
+                                }
+                                else
+                                {
+                                    if (skobki.Peek() == "}")
+                                    {
+                                        skobki.Pop();
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Скобки расставлены не правильно");
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                if (str[i] == ']')
+                                {
+                                    if (skobki.Count == 0)
+                                    {
+                                        Console.WriteLine("Скобки расставлены не правильно");
+                                    }
+                                    else
+                                    { 
+                                        if (skobki.Peek() == "]")
+                                        {
+                                            skobki.Pop();
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("Скобки расставлены не правильно");
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    if (str[i] == ')')
+                                    {
+                                        if (skobki.Count == 0)
+                                        {
+                                            Console.WriteLine("Скобки расставлены не правильно");
+                                        }
+                                        else
+                                        {
+                                            if (skobki.Peek() == ")")
+                                            {
+                                                skobki.Pop();
+                                            }
+                                            else
+                                            {
+                                                Console.WriteLine("Скобки расставлены не правильно");
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (skobki.Count!=0)
+            {
+                Console.WriteLine("Скобки расставлены не правильно");
+            }
+            else
+            {
+                if (skobki.Count == 0)
+                {
+                    Console.WriteLine("Скобки расставлены правильно");
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
